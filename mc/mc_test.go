a56e9fe3e30999725e90fa625f1378251637cc5b
Laurent Demailly <ldemailly@gmail.com>
2023-01-28 14:08:26 -0800

    adding [::1] resolve test

--- mc/mc_test.go
+++ mc/mc_test.go
@@ -1,10 +1,12 @@
 package mc_test
 
 import (
+	"context"
 	"os"
 	"testing"
 
 	"github.com/fortio/multicurl/cli"
+	"github.com/fortio/multicurl/mc"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
@@ -16,4 +18,21 @@ func TestMain(m *testing.M) {
 
 func TestMulticurl(t *testing.T) {
 	testscript.Run(t, testscript.Params{Dir: "../cli/"})
+}
+
+func TestResolveIP6(t *testing.T) {
+	p, a, err := mc.ResolveAll(context.Background(), "[::1]", "https", "ip")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if p != 443 {
+		t.Errorf("Unexpected port: %d", p)
+	}
+	if len(a) != 1 {
+		t.Errorf("Unexpected number of addresses: %d", len(a))
+	}
+	aStr := a[0].String()
+	if aStr != "::1" {
+		t.Errorf("Unexpected address: %s", aStr)
+	}
 }
