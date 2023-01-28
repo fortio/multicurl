4b24ba319b2f84b8ea5808fd545c5ee80b30506e
Laurent Demailly <ldemailly@gmail.com>
2023-01-28 13:40:24 -0800

    moving things around attempting to get some coverage

--- cli/multicurl_test.go
+++ cli/multicurl_test.go
@@ -1,15 +1,16 @@
-package main
+package cli_test
 
 import (
 	"os"
 	"testing"
 
+	"github.com/fortio/multicurl/cli"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(testscript.RunMain(m, map[string]func() int{
-		"multicurl": Main,
+		"multicurl": cli.Main,
 	}))
 }
 
--- multicurl.go
+++ multicurl.go
@@ -15,73 +15,11 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"net/http"
 	"os"
-	"time"
 
-	"fortio.org/fortio/log"
-	"fortio.org/fortio/version"
-	"github.com/fortio/multicurl/mc"
+	"github.com/fortio/multicurl/cli"
 )
-
-var (
-	fullVersion = flag.Bool("version", false, "Show full version info and exit.")
-	shortV      string
-)
-
-func usage(msg string, args ...any) {
-	_, _ = fmt.Fprintf(os.Stderr, "Fortio multicurl %s usage:\n\t%s [flags] url\nflags:\n",
-		shortV,
-		os.Args[0])
-	flag.PrintDefaults()
-	if msg != "" {
-		fmt.Fprintf(os.Stderr, msg, args...)
-		fmt.Fprintln(os.Stderr)
-	}
-}
 
 func main() {
-	os.Exit(Main())
-}
-
-// Main is the main function for the multicurl tool so it can be called from testscript.
-// Note that we could use the (new in 1.39) log.Fatalf that doesn't panic for cli tools but
-// it calling os.Exit directly means it doesn't work with the code coverage from `testscript`.
-func Main() int {
-	ipv4 := flag.Bool("4", false, "Only IPv4")
-	ipv6 := flag.Bool("6", false, "Only IPv6")
-	method := flag.String("method", http.MethodGet, "HTTP method")
-	totalTimeout := flag.Duration("total-timeout", 30*time.Second, "HTTP method")
-	requestTimeout := flag.Duration("request-timeout", 3*time.Second, "HTTP method")
-	flag.CommandLine.Usage = func() { usage("") }
-	log.SetFlagDefaultsForClientTools()
-	sV, longV, fullV := version.FromBuildInfo()
-	shortV = sV
-	flag.Parse()
-	if *fullVersion {
-		fmt.Print(fullV)
-		return 0
-	}
-	resolveType := "ip"
-	if !(*ipv4 && *ipv6) {
-		if *ipv4 {
-			resolveType = "ip4"
-		}
-		if *ipv6 {
-			resolveType = "ip6"
-		}
-	}
-	numArgs := len(flag.Args())
-	if numArgs != 1 {
-		usage("Need 1 argument (url), got %d (%v)", numArgs, flag.Args())
-		return 1
-	}
-	url := flag.Args()[0]
-	log.Infof("Fortio multicurl %s, using resolver %s, %s %s", longV, resolveType, *method, url)
-	ctx, cncl := context.WithTimeout(context.Background(), *totalTimeout)
-	defer cncl()
-	return mc.MultiCurl(ctx, *requestTimeout, *method, url, resolveType)
+	os.Exit(cli.Main())
 }
